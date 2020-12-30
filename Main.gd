36d3d44b57e992901722beec497fc74d07ad0169
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-29 20:21:48 -0500

    Set up the demo

--- Main.gd
+++ Main.gd
@@ -2,12 +2,12 @@ extends Control
 
 
 func _friendly():
-	return "> Greetings"
+	return "Greetings?"
 
 func _ready():
 	$Greetings.text = _friendly()
 
-
 func _on_Reload_timeout():
 	ProjectSettings.load_resource_pack("end.pck")
+	ResourceLoader.load("res://Main.gd", "", true).take_over_path("res://Main.gd")
 	get_tree().change_scene("res://Main.tscn")
